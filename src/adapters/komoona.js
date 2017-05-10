5ff68f0db4d25ae674a00592699b23cf419320cd
Darijusch Behbahani <darijusch.behbahani@researchgate.net>
2017-05-10 14:15:30 +0200

    updated komoona adapter

--- src/adapters/komoona.js
+++ src/adapters/komoona.js
@@ -3,6 +3,7 @@ var bidfactory = require('../bidfactory.js');
 var bidmanager = require('../bidmanager.js');
 var adloader = require('../adloader');
 var STATUS = require('../constants').STATUS;
+//Adapter version: 0.81
 
 var KomoonaAdapter = function KomoonaAdapter() {
   var KOMOONA_BIDDER_NAME = 'komoona';
@@ -12,7 +13,8 @@ var KomoonaAdapter = function KomoonaAdapter() {
       ts_as: new Date().getTime(),
       hb_placements: [],
       hb_placement_bidids: {},
-      kb_callback: _bid_arrived
+      kb_callback: _bid_arrived,
+      hb_floors: {}
     };
 
     var bids = params.bids || [];
@@ -24,7 +26,10 @@ var KomoonaAdapter = function KomoonaAdapter() {
       kbConf.hdbdid = kbConf.hdbdid || currentBid.params.hbid;
       kbConf.encode_bid = kbConf.encode_bid || currentBid.params.encode_bid;
       kbConf.hb_placement_bidids[currentBid.params.placementId] = currentBid.bidId;
-      kbConf.hb_placements.push(currentBid.params.placementId);
+      if (currentBid.params.floorPrice) {
+        kbConf.hb_floors[currentBid.params.placementId] = currentBid.params.floorPrice;
+      }
+      kbConf.hb_placements.push(currentBid.params.placementId);      
     });
 
     var scriptUrl = `//s.komoona.com/kb/0.1/kmn_sa_kb_c.${kbConf.hdbdid}.js`;
